93ff0b6a29b0eddd7b1f41b795c90137c6f43095
esmailCorp <chris@curotto.click>
2018-01-13 05:31:20 -0500

    boxBlur

--- src/BoxBlur.java
+++ src/BoxBlur.java
@@ -1,5 +1,36 @@
+import java.util.ArrayDeque;
+
 public class BoxBlur {
-    public static void main(String[] args) {
-        boxBlur() {
+    private static int[][] boxBlur(int[][] image) {
+        int[][] answer = new int[image.length-2][image[0].length-2];
+        ArrayDeque values = new ArrayDeque();
+
+        int jIndex = 0;
+        int iIndex = 0;
+
+        while (iIndex < image.length-2 && jIndex < image[0].length-2) {
+            int sum = 0;
+            int numElements = 0;
+            for (int i = iIndex; i < iIndex+3; i++) {
+                for (int j = jIndex; j < jIndex+3; j++) {
+                    sum += image[i][j];
+                    numElements++;
+                }
+            }
+            int avg = sum / numElements;
+            values.add(avg);
+            if(jIndex+3 == image[0].length){
+                iIndex++;
+                jIndex = 0;
+            } else {
+                jIndex++;
+            }
+        }
+        for (int i = 0; i < answer.length; i++) {
+            for (int j = 0; j < answer[i].length; j++) {
+                answer[i][j] = (int) values.remove();
+            }
+        }
+        return answer;
     }
 }
