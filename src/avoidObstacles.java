dec6cc01581ecdd4e89f4565b4af19a21c3e1cac
esmailCorp <chris@curotto.click>
2018-01-12 21:14:23 -0500

    avoidObstacles

---
+++ src/avoidObstacles.java
@@ -0,0 +1,33 @@
+public class avoidObstacles {
+    int avoidObstacles(int[] inputArray) {
+
+        int[] numberLine = new int[40];
+
+        for (int anInputArray : inputArray) {
+            numberLine[anInputArray] = 1;
+        }
+
+        int a = -1;
+        int b = -1;
+        int jump = 0;
+
+        for (int i = 0; i < numberLine.length; i++) {
+            if (numberLine[i] == 0) {
+                if(a == -1)
+                    a = i;
+                else if (b == -1)
+                    b = i;
+            }
+            jump = b - a;
+            if (jump > 0) {
+                for (int j = 0; j < numberLine.length; j+=jump) {
+                    if (numberLine[j] == 1) {
+                        b = -1;
+                        break;
+                    }
+                }
+            }
+        }
+        return jump;
+    }
+}
