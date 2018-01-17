268c4b73b9c42bdc06a5e2407d31782a6b07143c
esmailCorp <chris@curotto.click>
2018-01-16 19:32:25 -0500

    boxBlur

---
+++ src/ArithmeticExpression.java
@@ -0,0 +1,7 @@
+public class ArithmeticExpression {
+
+    boolean arithmeticExpression(int a, int b, int c) {
+        return a + b == c || a - b == c || a * b == c || a / b == c && a % b == 0;
+
+    }
+}
