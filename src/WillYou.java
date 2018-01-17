d40673fa3f1cb2fe51e845feac58fea8147b4692
esmailCorp <chris@curotto.click>
2018-01-17 11:01:46 -0500

    boxBlur

---
+++ src/WillYou.java
@@ -0,0 +1,7 @@
+public class WillYou {
+
+    boolean willYou(boolean young, boolean beautiful, boolean loved) {
+        return ((young && beautiful && !loved)) || (loved && (!young || !beautiful));
+
+    }
+}
